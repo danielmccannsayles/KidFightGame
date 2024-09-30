55f105318fbf8337769d127b369b5baf14f5df1a
Devin Hill <devin-hill@comcast.net>
2024-09-29 17:47:55 -0700

    Lil characters that can move and attack

---
+++ game_stuff/data/classes/Square.py
@@ -0,0 +1,62 @@
+import pygame
+
+class Square:
+	def __init__(self, x, y, width, height):
+		self.x = x
+		self.y = y
+		self.width = width
+		self.height = height
+
+		self.abs_x = x * width
+		self.abs_y = y * height
+		self.abs_pos = (self.abs_x, self.abs_y)
+		self.pos = (x, y)
+		self.color = 'light' if (x + y) % 2 == 0 else 'dark'
+		self.draw_color = (241, 211, 170) if self.color == 'light' else (180, 126, 82)
+		self.highlight_color = (150, 255, 100) if self.color == 'light' else (50, 220, 0)
+		self.occupying_piece = None
+		self.coord = self.get_coord()
+		self.highlight = False
+		self.attack_highlight = False
+		self.number_font  = pygame.font.SysFont( None, 16 )
+		self.hp_color = (220,20,60)
+		self.hp_rect = pygame.Rect(
+			self.abs_x,
+			self.abs_y,
+			self.width,
+			10
+		)
+		self.rect = pygame.Rect(
+			self.abs_x,
+			self.abs_y,
+			self.width,
+			self.height
+		)
+
+
+
+
+	def get_coord(self):
+		columns = 'abcdefgh'
+		return columns[self.x] + str(self.y + 1)
+
+
+	def draw(self, display):
+		if self.highlight:
+			pygame.draw.rect(display, self.highlight_color, self.rect)
+		elif self.attack_highlight:
+			pygame.draw.rect(display, self.hp_color, self.rect)
+		else:
+			pygame.draw.rect(display, self.draw_color, self.rect)
+
+		if self.occupying_piece != None:
+			centering_rect = self.occupying_piece.img.get_rect()
+			centering_rect.center = self.rect.center
+			display.blit(self.occupying_piece.img, centering_rect.topleft)
+			pygame.draw.rect(display, self.hp_color, self.hp_rect)
+			hptext = str(self.occupying_piece.hp)
+			number_image = self.number_font.render( hptext, True, (10,10,10), (255,255,255) )
+			display.blit(number_image, centering_rect.topleft)
+
+
+
---
+++ game_stuff/data/classes/pieces/Rook.py
@@ -0,0 +1,45 @@
+import pygame
+
+from data.classes.Piece import Piece
+
+class Rook(Piece):
+	def __init__(self, pos, color, board, movespeed, hp, attack_dmg):
+		super().__init__(pos, color, board)
+
+		if(self.color) == 'white':
+			img_path = 'data/imgs/brutal-helm.png'
+		else:
+			img_path = 'data/imgs/orc-head.png'
+
+		self.img = pygame.image.load(img_path)
+		self.img = pygame.transform.scale(self.img, (board.square_width - 20, board.square_height - 20))
+
+		self.notation = 'R'
+
+
+		self.movespeed = movespeed
+		self.max_hp = hp
+		self.hp = self.max_hp
+
+		self.attack_dmg = attack_dmg
+		
+
+	def get_possible_moves(self, board):
+		output = []
+		moves_north = []
+		y = range(self.y - self.movespeed, self.y + self.movespeed + 1)
+		x = range(self.x - self.movespeed, self.x + self.movespeed + 1)
+		print(*x, sep=",")
+		for t in y:
+			if (t>=0) and (t<8) and not (t==self.y):
+				moves_north.append(board.get_square_from_pos(
+				(self.x, t)))
+		output.append(moves_north)
+		moves_east = []
+		for t in x:
+			if (t>=0) and (t<8) and not (t==self.x):
+				moves_east.append(board.get_square_from_pos(
+					(t, self.y)
+				))
+		output.append(moves_east)
+		return output
